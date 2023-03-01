e55304c178a1a149f9dcd362b8aec5d28013106e
yehor <ivashin.egor98@gmail.com>
2023-03-01 13:39:08 +0200

    +++

--- validationName/validationName.js
+++ validationName/validationName.js
@@ -1,4 +1,4 @@
-function validationName(inputName) {
+export function validationName(inputName) {
   const name = inputName;
   const nameRegexp = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/;
   validation(name, nameRegexp);
