bdfa33191080a0a5b7967090e350dac63d729b6d
yehor <ivashin.egor98@gmail.com>
2023-03-01 13:17:15 +0200

    +++

--- allValidation.js
+++ allValidation.js
@@ -1,4 +1,4 @@
-export function validationName(inputName) {
+function validationName(inputName) {
   const name = inputName;
   const nameRegexp = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/;
   validation(name, nameRegexp);
@@ -17,4 +17,4 @@ export function validationEmail(inputEmail) {
   validation(email, phoneRegexp);
 }
 
-console.log('fdfdf');
+export default validationName;
