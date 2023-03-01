b37dcace03b90c6f48b9bb8252b51af7ced4b6a1
yehor <ivashin.egor98@gmail.com>
2023-03-01 14:21:28 +0200

    +++

---
+++ validation.js
@@ -0,0 +1,28 @@
+function validationName(inputName) {
+  const name = inputName;
+  const nameRegexp = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/;
+  validation(name, nameRegexp);
+}
+
+function validationNumberPhone(numberPhone) {
+  const phone = numberPhone;
+  const phoneRegexp = /(\d?)(\d{3})(\d{3})(\d{2})(\d{2})/g;
+  validation(phone, phoneRegexp);
+}
+
+function validationEmail(inputEmail) {
+  const email = inputEmail;
+  const phoneRegexp =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  validation(email, phoneRegexp);
+}
+
+function validation(input, regexp) {
+  let isValid = false;
+  if (regexp.test(input)) {
+    isValid = true;
+  } else {
+    throw new Error('Вы ввели некоректные данные');
+  }
+  return isValid;
+}
--- validationForm.js
+++
@@ -1,9 +0,0 @@
-export function validation(input, regexp) {
-  let isValid = false;
-  if (regexp.test(input)) {
-    isValid = true;
-  } else {
-    throw new Error('Вы ввели некоректные данные');
-  }
-  return isValid;
-}
