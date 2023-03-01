272e6cce5a0a66de7985a51a104abffb1889c451
yehor <ivashin.egor98@gmail.com>
2023-03-01 14:16:48 +0200

    +++

--- allValidation.js
+++ allValidation.js
@@ -1,6 +1,4 @@
-import { validation } from './validationForm';
-
-export function validationName(inputName) {
+function validationName(inputName) {
   const name = inputName;
   const nameRegexp = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/;
   validation(name, nameRegexp);
@@ -18,3 +16,13 @@ export function validationName(inputName) {
 //     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 //   validation(email, phoneRegexp);
 // }
+
+function validation(input, regexp) {
+  let isValid = false;
+  if (regexp.test(input)) {
+    isValid = true;
+  } else {
+    throw new Error('Вы ввели некоректные данные');
+  }
+  return isValid;
+}
